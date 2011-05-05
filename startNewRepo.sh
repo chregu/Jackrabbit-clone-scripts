1a7c6ad88a6694d9758386c7db04c91672c3da88
Christian Stocker <me@chregu.tv>
2011-05-05 14:16:06 +0200

    Don't overwrite already existsing JOURNAL_ID in DB

--- startNewRepo.sh
+++ startNewRepo.sh
@@ -17,15 +17,18 @@ fi
 ID=`cat $REPOLOCATION/current_revision_id.dat`
 
 
-echo "Check if JOURNAL_ID $JRCLUSTERID_NEW already exists"
+echo "Check if JOURNAL_ID '$JRCLUSTERID_NEW' already exists"
 CLUSTERIDCHECK=`$MYSQLBIN -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS $MYSQLDB --silent -N -e "select REVISION_ID from JOURNAL_LOCAL_REVISIONS where JOURNAL_ID = '$JRCLUSTERID_NEW'"`
 if [[ -z $CLUSTERIDCHECK ]]
 then
 echo "It does not exist, insert it and put REVISION_ID  to $ID"
 $MYSQLBIN -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS $MYSQLDB --silent -N -e "INSERT INTO JOURNAL_LOCAL_REVISIONS (REVISION_ID, JOURNAL_ID) VALUES($ID, '$JRCLUSTERID_NEW');"
 else
-echo "It exists, update REVISION_ID to $ID"
-$MYSQLBIN -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS $MYSQLDB --silent -N -e "UPDATE JOURNAL_LOCAL_REVISIONS SET REVISION_ID = $ID where  JOURNAL_ID = '$JRCLUSTERID_NEW';"
+echo "!!!!!!"
+echo "JOURNAL_ID '$JRCLUSTERID_NEW' already exists"
+echo "Please delete it by hand from the database (or choose another JOURNAL_ID) to prevent accidental screw-ups with your Journal"
+echo "!!!!!!"
+exit 3
 fi
 
 
